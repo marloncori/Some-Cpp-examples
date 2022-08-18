6a208832718cd890f06ff555032dc2e6288da9c8
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-08-18 12:13:00 +0200

    Update Arduino.h

--- obstacle_avoiding_robot/Arduino.h
+++ obstacle_avoiding_robot/Arduino.h
@@ -18,37 +18,42 @@
 
 std::string line(" ----------------------------------------------------");
 
-void pinMode(unsigned int GPIO, unsigned int direction){
+template<class T, class U>
+void pinMode(T GPIO, U direction){
   std::cout << "\n" << line << std::endl;
   std::cout << "  An electronic component has been connected to \n   Arduino\'s GPIO "
    << GPIO << " and it is configured as " << (direction == OUTPUT ? "OUTPUT" : "INPUT") << "!" << std::endl;	
   std::cout << line << "\n" << std::endl;
 }
 
-void digitalWrite(unsigned int GPIO, unsigned int logic){
+template<class T, class U>
+void digitalWrite(T GPIO, U logic){
   std::cout << "\n\t" << line << std::endl;
   std::cout << "\t   [DIGITAL INFO] Arduino\'s GPIO " << GPIO << " is set to " << 
    (logic == LOW ? "LOW" : "HIGH") << "!" << std::endl;	
   std::cout << "\t" << line << "\n" << std::endl;
 }
 
-void analogWrite(unsigned int GPIO, long bits){
+template<class T, class U>
+void analogWrite(T GPIO, U bits){
   std::cout << "\n" << line << std::endl;
   std::cout << "  [ANALOG INFO] A value of " << bits << " bits has been written \n           to Arduino\'s GPIO number " << GPIO << "!" << std::endl;	
   std::cout << line << "\n" << std::endl;	
 }
 
-void delayMicroseconds(long time){
+template<class T>
+void delayMicroseconds(T time){
     std::cout << "  [ARDUINO] Board has been told to wait for " << time << " microseconds." << std::endl;	
 	Sleep(time);
 }
 
-unsigned long pulseIn(unsigned int GPIO, unsigned int state, unsigned long timeout){
-	unsigned long width = 0; // keep initialization out of time critical area
+template<class T, class L, class U>
+U pulseIn(T GPIO, L state, U timeout){
+	U width = 0; // keep initialization out of time critical area
 	// convert the timeout from microseconds to a number of times through
 	// the initial loop; it takes 16 clock cycles per iteration.
-	unsigned long numloops = 0;
-	unsigned long maxloops = microsecondsToClockCycles(timeout) / 16;
+	U numloops = 0;
+	U maxloops = microsecondsToClockCycles(timeout) / 16;
 	
 	do{
 		++numloops;
