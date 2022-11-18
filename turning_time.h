bcdf7689897c9dac0067d562fe6333e4e885d242
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-11-18 02:29:11 +0100

    Create turning_time.h

---
+++ turning_time.h
@@ -0,0 +1,11 @@
+#ifndef TURNING_TIME_H_
+#define TURNING_TIME_H_
+
+#define MAX_ANGLE 180
+
+enum TurningTime {
+	MAX_ARRIVAL_TIME    = 800,
+	ARRIVAL_TIME_OFFSET = MAX_ARRIVAL_TIME/MAX_ANGLE,	
+};
+
+#endif
